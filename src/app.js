73f9c23143ea204d9682ccaa104924f86b0e1053
Mark Gerarts <mark.gerarts@gmail.com>
2025-09-10 21:50:38 +0200

    Shabby mix of alpine and manual svg rendering because of <template> limitations...

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import data from './data';
+import { renderSkillTreeSvg } from './renderer';
 
 export default () => ({
   data,
   activeSection: data.sections[0],
+  selectedSkill: null,
+
+  init() {
+  },
 
   setActiveSection(sectionId) {
     this.activeSection = data.getSection(sectionId);
+  },
+
+  renderSkillTreeToHtml(skillTree) {
+    return renderSkillTreeSvg(skillTree).outerHTML;
+  },
+
+  setSelectedSkill(skillId) {
+    for (const skillTree of this.activeSection.skillTrees) {
+      for (const skill of skillTree.skills) {
+        if (skill.id === skillId) {
+          console.log(skill);
+          this.selectedSkill = skill;
+        }
+      }
+    }
   }
 });
--- src/data.js
+++ src/data.js
@@ -8,27 +8,27 @@ export default {
           id: 'unarmed',
           name: 'Unarmed',
           skills: [
-            { id: "FistsOfFury", dependsOn: [], position: [335, 689] },
-            { id: "BlowsOfTheSnake", dependsOn: ["FistsOfFury"], position: [197, 544] },
-            { id: "Brawler", dependsOn: ["FistsOfFury"], position: [483, 544] },
-            { id: "IronHands", dependsOn: ["Brawler"], position: [381, 544] },
+            { id: "FistsOfFury", name: "Fists of Fury", dependsOn: [], position: { x: 335, y: 689 } },
+            { id: "BlowsOfTheSnake", name: "Blows of the Snake", dependsOn: ["FistsOfFury"], position: { x: 197, y: 544 } },
+            { id: "Brawler", name: "Brawler", dependsOn: ["FistsOfFury"], position: { x: 483, y: 544 } },
+            { id: "IronHands", name: "Iron Hands", dependsOn: ["Brawler"], position: { x: 381, y: 544 } },
           ]
         },
         {
           id: 'twoHanded',
-          name: 'Two-Handed',
+          name: 'Two Handed',
           skills: [
-            { id: "BrutishFighter", dependsOn: [], position: [636, 375] },
-            { id: "MightyBlows", dependsOn: ["BrutishFighter"], position: [556, 304] },
-            { id: "PerpetualSwings", dependsOn: ["MightyBlows"], position: [428, 304] },
-            { id: "FuryWithoutFatigue", dependsOn: ["BrutishFighter"], position: [723, 304] },
-            { id: "EnduringStrikes", dependsOn: ["FuryWithoutFatigue"], position: [842, 304] },
-            { id: "GiantsGrace", dependsOn: ["FuryWithoutFatigue"], position: [786, 224] },
-            { id: "UnstoppableForce", dependsOn: ["BrutishFighter"], position: [636, 303] },
-            { id: "CrushingBlow", dependsOn: ["UnstoppableForce"], position: [520, 181] },
-            { id: "UnstoppableImpact", dependsOn: ["UnstoppableForce"], position: [636, 222] },
-            { id: "DominatingPresence", dependsOn: ["UnstoppableImpact"], position: [715, 138] },
-            { id: "CullingTheHerd", dependsOn: ["UnstoppableImpact"], position: [636, 69] },
+            { id: "BrutishFighter", name: "Brutish Fighter", dependsOn: [], position: { x: 636, y: 375 } },
+            { id: "MightyBlows", name: "Mighty Blows", dependsOn: ["BrutishFighter"], position: { x: 556, y: 304 } },
+            { id: "PerpetualSwings", name: "Perpetual Swings", dependsOn: ["MightyBlows"], position: { x: 428, y: 304 } },
+            { id: "FuryWithoutFatigue", name: "Fury Without Fatigue", dependsOn: ["BrutishFighter"], position: { x: 723, y: 304 } },
+            { id: "EnduringStrikes", name: "Enduring Strikes", dependsOn: ["FuryWithoutFatigue"], position: { x: 842, y: 304 } },
+            { id: "GiantsGrace", name: "Giant's Grace", dependsOn: ["FuryWithoutFatigue"], position: { x: 786, y: 224 } },
+            { id: "UnstoppableForce", name: "Unstoppable Force", dependsOn: ["BrutishFighter"], position: { x: 636, y: 303 } },
+            { id: "CrushingBlow", name: "Crushing Blow", dependsOn: ["UnstoppableForce"], position: { x: 520, y: 181 } },
+            { id: "UnstoppableImpact", name: "Unstoppable Impact", dependsOn: ["UnstoppableForce"], position: { x: 636, y: 222 } },
+            { id: "DominatingPresence", name: "Dominating Presence", dependsOn: ["UnstoppableImpact"], position: { x: 715, y: 138 } },
+            { id: "CullingTheHerd", name: "Culling the Herd", dependsOn: ["UnstoppableImpact"], position: { x: 636, y: 69 } },
           ]
         }
       ]
